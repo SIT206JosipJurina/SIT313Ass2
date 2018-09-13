b6ed46e005f8c493e219e1e773eb7db9aec4b162
Admin <Admin@JOSIP>
2018-09-13 23:23:23 +1000

    commit pass

--- SIT313Assignment2/SIT313Assignment2/Models/Constants.cs
+++ SIT313Assignment2/SIT313Assignment2/Models/Constants.cs
@@ -10,7 +10,7 @@ namespace SIT313Assignment2.Models
         public static bool IsDev = true;
 
         public static Color BackgroundColor = Color.FromRgb(58, 153, 215); // This is the background color // Is Set to Blue Currently 
-        public static Color MainTextColor = Color.White; // This is Main Text Color // Is Set to White Currently
+        public static Color MainTextColor = Color.White; // This is Main Text Color // Is Set to White Currently  
 
         public static int LoginIconHeight = 120;
     }
